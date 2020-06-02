04998926ca51468a1a64aed89e0fbf71756fba81
Maicon de Menezes <mdmoliveira@inf.ufpel.edu.br>
2020-06-01 22:27:15 -0300

    Updates

---
+++ precache-manifest.9e0e4cae5d39d282d9cd9a8d3110de7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff6d3a551ec26b9e927b9bd166c5909a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfe2530bf211e50307ad",
+    "url": "/static/css/main.53d4983c.chunk.css"
+  },
+  {
+    "revision": "86bfaebb4cdd33637e0d",
+    "url": "/static/js/2.2a8ab53f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.2a8ab53f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfe2530bf211e50307ad",
+    "url": "/static/js/main.c672f87a.chunk.js"
+  },
+  {
+    "revision": "a9120a0481c6a004c4c1",
+    "url": "/static/js/runtime-main.416bef8a.js"
+  }
+]);
